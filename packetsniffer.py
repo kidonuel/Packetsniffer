8c4178199b25ab056be7ebffc2911e92190f1f39
kidonuel <127172013+kidonuel@users.noreply.github.com>
2023-05-07 23:56:29 +0100

    Update packetsniffer.py

--- packetsniffer.py
+++ packetsniffer.py
@@ -1,13 +1,34 @@
 import argparse
 from scapy.all import *
+from scapy.layers.inet import TCP
+from scapy.layers.inet import IP
 
-parser = argparse.ArgumentParser(description='Simple network sniffer')
-parser.add_argument('-i', '--interface', help='Network interface to sniff on', required=True)
-parser.add_argument('-t', '--timeout', help='Timeout for the sniff function', default=10)
-parser.add_argument('-f', '--filter', help='BPF filter for the sniff function', default='tcp port 80')
-args = parser.parse_args()
+
+def http_header(packet):
+    http_packet = str(packet)
+    if http_packet.find('GET'):
+        return 'GET'
+    elif http_packet.find('POST'):
+        return 'POST'
+    else:
+        return ''
 
 def packet_callback(packet):
-    print(packet.summary())
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+        if http_header(packet):
+            print('[+] HTTP Request: ' + str(packet[IP].src) + ' -> ' + str(packet[IP].dst) + ' ' + http_header(packet))
 
-sniff(iface=args.interface, timeout=int(args.timeout), filter=args.filter, prn=packet_callback)
+parser = argparse.ArgumentParser(description='Python packet sniffer')
+parser.add_argument('-i', '--interface', metavar='', required=True, help='Interface to capture packets on')
+parser.add_argument('-o', '--output', metavar='', help='File to save captured packets')
+args = parser.parse_args()
+
+try:
+    if args.output:
+        print('[+] Saving packets to file: ' + args.output)
+        sniff(iface=args.interface, prn=packet_callback, filter='tcp port 80', store=0, count=0, offline=args.output)
+    else:
+        sniff(iface=args.interface, prn=packet_callback, filter='tcp port 80', store=0)
+except KeyboardInterrupt:
+    print('\n[+] User interrupted. Exiting...')
